07446abbf30e60543ffc54a2b0c93171a11842b7
Caceresenzo <caceresenzo1502@gmail.com>
2023-12-23 19:42:10 +0100

    pass 4th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
 public class Main {
@@ -10,28 +8,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		System.out.println("codecrafters build-your-own-redis");
 
+		final var threadFactory = Thread.ofVirtual().factory();
+
 		try (final var serverSocket = new ServerSocket(PORT)) {
 			serverSocket.setReuseAddress(true);
 
-			final var client = serverSocket.accept();
+			while (true) {
+				final var client = serverSocket.accept();
 
-			final var reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			final var outputStream = client.getOutputStream();
-
-			String line;
-			while ((line = reader.readLine()) != null) {
-				//				System.out.println(line);
-
-				if ("DOCS".equalsIgnoreCase(line)) {
-					outputStream.write("*0\r\n".getBytes());
-				} else if ("PING".equalsIgnoreCase(line)) {
-					outputStream.write("+PONG\r\n".getBytes());
-				}
-
-				outputStream.flush();
+				final var thread = threadFactory.newThread(new Client(client));
+				thread.start();
 			}
-
-			client.close();
 		}
 	}
 
