851ff115a6c663cde002504036ac171bbd631305
Caceresenzo <caceresenzo1502@gmail.com>
2024-11-10 22:01:34 +0100

    CLI tests (cli-test-1731272494837)

--- src/main/java/redis/serial/Serializer.java
+++ src/main/java/redis/serial/Serializer.java
@@ -5,10 +5,14 @@ import java.io.OutputStream;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
-import redis.type.BulkBlob;
-import redis.type.BulkString;
-import redis.type.Error;
-import redis.type.Ok;
+import redis.type.RArray;
+import redis.type.RBlob;
+import redis.type.RError;
+import redis.type.RInteger;
+import redis.type.RNil;
+import redis.type.ROk;
+import redis.type.RString;
+import redis.type.RValue;
 
 @RequiredArgsConstructor
 public class Serializer {
@@ -23,133 +27,96 @@ public class Serializer {
 		outputStream.flush();
 	}
 
-	public boolean write(Object value) throws IOException {
-		if (value instanceof String string) {
-			if (string.contains("\r\n")) {
-				return writeBulkString(string);
+	public void write(RValue value) throws IOException {
+		switch (value) {
+			case RArray<?> array -> writeArray(array.items());
+			case RBlob blob -> {
+				if (!blob.bulk()) {
+					throw new UnsupportedOperationException("non bulk blob are not supported");
+
+				}
+
+				writeBulkBytes(blob.content());
 			}
-
-			return writeSimpleString(string);
+			case RError error -> writeError(error);
+			case RInteger integer -> writeSimpleInteger(integer.value());
+			case RNil nil -> {
+				if (nil.bulk()) {
+					writeNullBulk();
+				} else {
+					writeNil();
+				}
+			}
+			case ROk ok -> writeOk();
+			case RString string -> {
+				if (string.bulk()) {
+					writeBulkString(string.content());
+				} else {
+					writeSimpleString(string.content());
+				}
+			}
 		}
-
-		if (value instanceof Integer integer) {
-			return writeSimpleInteger(integer);
-		}
-
-		if (value instanceof BulkString string) {
-			return writeBulkString(string.message());
-		}
-
-		if (value instanceof BulkBlob string) {
-			return writeBulkBytes(string.message());
-		}
-
-		if (value instanceof List<?> list) {
-			return writeArray(list);
-		}
-
-		if (value instanceof Error error) {
-			return writeError(error);
-		}
-
-		if (value instanceof Ok) {
-			return writeOk();
-		}
-
-		if (value == null) {
-			return writeNil();
-		}
-
-		throw new UnsupportedOperationException("type " + value.getClass().getSimpleName());
 	}
 
-	private boolean writeSimpleString(String string) throws IOException {
+	private void writeSimpleString(String string) throws IOException {
 		outputStream.write(Protocol.SIMPLE_STRING);
 		outputStream.write(string.getBytes());
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeSimpleInteger(Integer integer) throws IOException {
+	private void writeSimpleInteger(Integer integer) throws IOException {
 		outputStream.write(Protocol.INTEGER);
 		outputStream.write(String.valueOf(integer).getBytes());
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeBulkString(String string) throws IOException {
-		if (string == null) {
-			return writeNullBulk();
-		}
-
+	private void writeBulkString(String string) throws IOException {
 		writeBulkBytes(string.getBytes());
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeBulkBytes(byte[] bytes) throws IOException {
-		if (bytes == null) {
-			return writeNullBulk();
-		}
-
+	private void writeBulkBytes(byte[] bytes) throws IOException {
 		outputStream.write(Protocol.BULK_STRING);
 
 		outputStream.write(String.valueOf(bytes.length).getBytes());
 		outputStream.write(CRLF_BYTES);
 
 		outputStream.write(bytes);
-
-		return true;
 	}
 
-	private boolean writeNullBulk() throws IOException {
+	private void writeNullBulk() throws IOException {
 		outputStream.write(Protocol.BULK_STRING);
 		outputStream.write(MINUS_ONE_BYTES);
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeArray(List<?> list) throws IOException {
+	private void writeArray(List<? extends RValue> list) throws IOException {
 		outputStream.write(Protocol.ARRAY);
 		outputStream.write(String.valueOf(list.size()).getBytes());
 		outputStream.write(CRLF_BYTES);
 
 		for (final var element : list) {
-			if (!write(element)) {
-				return false;
-			}
+			write(element);
 		}
-
-		return true;
 	}
 
-	private boolean writeError(Error error) throws IOException {
+	private void writeError(RError error) throws IOException {
 		final var message = error.message();
 
 		outputStream.write(Protocol.SIMPLE_ERROR);
 		outputStream.write(message.getBytes());
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeOk() throws IOException {
+	private void writeOk() throws IOException {
 		outputStream.write(Protocol.SIMPLE_STRING);
 		outputStream.write(OK_BYTES);
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
-	private boolean writeNil() throws IOException {
+	private void writeNil() throws IOException {
 		outputStream.write(Protocol.NULL);
 		outputStream.write(CRLF_BYTES);
-
-		return true;
 	}
 
 }
--- src/main/java/redis/type/BulkBlob.java
+++
@@ -1,3 +0,0 @@
-package redis.type;
-
-public record BulkBlob(byte[] message) {}
