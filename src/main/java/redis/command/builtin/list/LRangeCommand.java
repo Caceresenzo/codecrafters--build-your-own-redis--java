b7ea7d5156f6506b0742708bca5bb4b01ace731a
Caceresenzo <caceresenzo1502@gmail.com>
2025-07-19 17:32:06 +0200

    pass 5th lists

--- src/main/java/redis/command/builtin/list/LRangeCommand.java
+++ src/main/java/redis/command/builtin/list/LRangeCommand.java
@@ -21,10 +21,22 @@ public record LRangeCommand(
 		}
 
 		final var size = array.size();
-		final var start = Math.min(startIndex, size);
-		final var end = Math.min(endIndex + 1, size);
+		final var start = scope(startIndex, size);
+		final var end = scope(endIndex, size);
 
-		return new CommandResponse(RArray.view(array.subList(start, end)));
+		if (start > end) {
+			return new CommandResponse(RArray.empty());
+		}
+
+		return new CommandResponse(RArray.view(array.subList(start, end + 1)));
+	}
+
+	private Integer scope(int index, int size) {
+		if (index < 0) {
+			index += size;
+		}
+
+		return Math.clamp(index, 0, size - 1);
 	}
 
 }
