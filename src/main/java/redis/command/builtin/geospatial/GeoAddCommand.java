08032d5b101da1ff178b00d31ee951842ff1ec67
Caceresenzo <caceresenzo1502@gmail.com>
2025-08-31 16:38:58 +0200

    CLI tests (cli-test-1756651137893)

--- src/main/java/redis/command/builtin/geospatial/GeoAddCommand.java
+++ src/main/java/redis/command/builtin/geospatial/GeoAddCommand.java
@@ -4,19 +4,19 @@ import redis.Redis;
 import redis.client.Client;
 import redis.command.Command;
 import redis.command.CommandResponse;
+import redis.type.GeoCoordinate;
 import redis.type.RInteger;
 import redis.type.RString;
 
 public record GeoAddCommand(
 	RString key,
-	double longitude,
-	double latitude,
+	GeoCoordinate coordinate,
 	RString member
 ) implements Command {
 
 	@Override
 	public CommandResponse execute(Redis redis, Client client) {
-		final var score = 0;
+		final var score = coordinate.encode();
 
 		redis.getStorage().addToSet(
 			key.content(),
