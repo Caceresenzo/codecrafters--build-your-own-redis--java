46c44497c71c47dbb18f9944fea214d3ba61fc98
Caceresenzo <caceresenzo1502@gmail.com>
2025-08-31 17:15:45 +0200

    CLI tests (cli-test-1756653345252)

---
+++ src/main/java/redis/command/builtin/geospatial/GeoPosCommand.java
@@ -0,0 +1,45 @@
+package redis.command.builtin.geospatial;
+
+import java.util.List;
+
+import redis.Redis;
+import redis.client.Client;
+import redis.command.Command;
+import redis.command.CommandResponse;
+import redis.type.RArray;
+import redis.type.RInteger;
+import redis.type.RNil;
+import redis.type.RString;
+
+public record GeoPosCommand(
+	RString key,
+	List<RString> members
+) implements Command {
+
+	@Override
+	public CommandResponse execute(Redis redis, Client client) {
+		final var sortedSet = redis.getStorage().getSortedSet(key.content());
+		if (sortedSet == null) {
+			final var scores = members.stream()
+				.map((__) -> RNil.ARRAY)
+				.toList();
+
+			return new CommandResponse(RArray.view(scores));
+		}
+
+		final var scores = members.stream()
+			.map(RString::content)
+			.map(sortedSet::getScore)
+			.map((score) -> {
+				if (score == null) {
+					return RNil.ARRAY;
+				}
+
+				return RArray.of(RString.bulk("0"), RString.bulk("0"));
+			})
+			.toList();
+
+		return new CommandResponse(RArray.view(scores));
+	}
+
+}
