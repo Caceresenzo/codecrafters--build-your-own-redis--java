321b4f9d2c827ccdb10df00409ea83110df7f304
Caceresenzo <caceresenzo1502@gmail.com>
2026-04-11 22:04:56 +0200

    pass 2nd authentication

---
+++ src/main/java/redis/command/builtin/acl/AclGetUserCommand.java
@@ -0,0 +1,27 @@
+package redis.command.builtin.acl;
+
+import redis.Redis;
+import redis.client.Client;
+import redis.command.Command;
+import redis.command.CommandResponse;
+import redis.type.RArray;
+import redis.type.RString;
+
+public record AclGetUserCommand(
+	RString username
+) implements Command {
+
+	@Override
+	public CommandResponse execute(Redis redis, Client client) {
+		return new CommandResponse(RArray.of(
+			RString.bulk(username),
+			RArray.empty()
+		));
+	}
+
+	@Override
+	public boolean isQueueable() {
+		return false;
+	}
+
+}
