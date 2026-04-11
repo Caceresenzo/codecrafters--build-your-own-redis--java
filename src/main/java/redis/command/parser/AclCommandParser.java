104218a99a37c7566c2b871ca6300664ddd83505
Caceresenzo <caceresenzo1502@gmail.com>
2026-04-11 22:04:56 +0200

    pass 2nd authentication

--- src/main/java/redis/command/parser/AclCommandParser.java
+++ src/main/java/redis/command/parser/AclCommandParser.java
@@ -1,11 +1,13 @@
 package redis.command.parser;
 
+import redis.command.builtin.acl.AclGetUserCommand;
 import redis.command.builtin.acl.AclWhoamiCommand;
 
 public class AclCommandParser extends CommandParser {
 
 	public AclCommandParser() {
 		register("WHOAMI", noArgumentCommand(AclWhoamiCommand::new));
+		register("GETUSER", singleArgumentCommand(AclGetUserCommand::new));
 	}
 
 }
