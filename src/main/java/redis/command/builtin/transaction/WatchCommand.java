5c0fb84288314fc41362d40828766560a1aba526
Caceresenzo <caceresenzo1502@gmail.com>
2026-04-11 23:07:38 +0200

    CLI tests (cli-test-1775941657975)

--- src/main/java/redis/command/builtin/transaction/WatchCommand.java
+++ src/main/java/redis/command/builtin/transaction/WatchCommand.java
@@ -2,9 +2,11 @@ package redis.command.builtin.transaction;
 
 import redis.Redis;
 import redis.client.Client;
+import redis.client.SocketClient;
 import redis.command.Command;
 import redis.command.CommandResponse;
 import redis.type.RArray;
+import redis.type.RError;
 import redis.type.ROk;
 import redis.type.RString;
 
@@ -12,8 +14,17 @@ public record WatchCommand(
 	RArray<RString> keys
 ) implements Command {
 
+	public static final RError IN_TRANSACTION = new RError("ERR WATCH inside MULTI is not allowed");
+
 	@Override
 	public CommandResponse execute(Redis redis, Client client) {
+
+		final var socketClient = SocketClient.cast(client);
+
+		if (socketClient.isInTransaction()) {
+			throw IN_TRANSACTION.asException();
+		}
+
 		return new CommandResponse(ROk.OK);
 	}
 
